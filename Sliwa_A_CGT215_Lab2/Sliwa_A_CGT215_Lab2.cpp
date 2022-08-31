13bb8750f4633dd48aa6625397c25d3f0bb8259d
Andy Sliwa <sliwaa@purdue.edu>
2022-08-31 11:45:59 -0400

    Add project files.

---
+++ Sliwa_A_CGT215_Lab2/Sliwa_A_CGT215_Lab2.cpp
@@ -0,0 +1,25 @@
+// Sliwa_A_CGT215_Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float A;
+	float B;
+	float X;
+
+	cout << "Hello, my name is Andy and I'm going to solve the equation:" << endl; 
+		//this is my intro to my code...
+	cout << "Ax + B = 0" << endl;
+	cout << "For x" << endl;
+		//I was able to make new lines by writing "<< endl" at the end... 
+		//to solve: how to seperate this code with another blank line?
+
+	cout << endl "Please enter a value for A: ";
+	cin >> A;
+	cout << "A=" << A << endl;
+	0 = A(X) + B;
+	cout << "The answer is:" << B << endl;
+}
+
